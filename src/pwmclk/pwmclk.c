9f6f7c3a3efac2bc7c24afec84c3a93ad7e14d77
Fraser <github@frasersdev.net>
2016-02-13 18:41:02 +1100

    Cleanup of pwmclk file.

--- src/pwmclk/pwmclk.c
+++ src/pwmclk/pwmclk.c
@@ -19,10 +19,18 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
  
-#define PWMCLK_MMAP			"/sys/firmware/devicetree/base/soc/ranges" 
+/* 
+ * This program starts up the pwm clock if ti isn't already running
+ *
+ * Do I need to stop the clock and start it again if it is already running? well time will tell I guess...
+ *
+ */
+
+ 
+#define PWMCLK_MMAP		"/sys/firmware/devicetree/base/soc/ranges" 
  
 #define PWMCLK_SPEED		0x00010000 // divide clock by 16
-#define PWMCLK_PASSWORD     (0x5A << 24) 	
+#define PWMCLK_PASSWORD     	(0x5A << 24) 	
 #define PWMCLK_REG_CTRL   	0x1010a0 	//https://www.scribd.com/doc/127599939/BCM2835-Audio-clocks
 #define PWMCLK_REG_DIV    	0x1010a4 	//https://www.scribd.com/doc/127599939/BCM2835-Audio-clocks
 
@@ -41,21 +49,14 @@
 #include <sys/mman.h>
 #include <sys/types.h>	
 
-//BCM2835_GPIO_BASE               0x200000
-//#define BCM2835_GPLEV0            0x0034
-
-//200034
 
 uint32_t get_and_map_mem();
 void start_pwmclk();
 uint32_t reg_read(volatile uint32_t* address);
 void reg_write(volatile uint32_t* address,uint32_t value);
 
- 
-
 uint32_t map_err;
 static volatile uint32_t*  mem_map = MAP_FAILED;
- 
  
  
 int main(int argc, char * argv[]) {
@@ -63,27 +64,23 @@ int main(int argc, char * argv[]) {
 	if(get_and_map_mem()) {
 		start_pwmclk();
 	}
-    return map_err ;
+	return map_err ;
 }
- 
- 
- 
- 
  
 uint32_t get_and_map_mem() {
 
-    int fd, dev;
+	int fd, dev;
 	uint8_t buf[4];
-    uint32_t mem_base, mem_size;
+	uint32_t mem_base, mem_size;
     
-    map_err=0;
-    mem_base=0;
+	map_err=0;
+	mem_base=0;
 	mem_size=0;
 
 	fd = open(PWMCLK_MMAP, O_RDONLY);
-    if(fd>=0)   {
-        lseek(fd, 4, SEEK_SET);
-        if (read(fd, buf, sizeof(buf)) == sizeof(buf)) {
+	if(fd>=0)   {
+		lseek(fd, 4, SEEK_SET);
+		if (read(fd, buf, sizeof(buf)) == sizeof(buf)) {
 			mem_base = (buf[0] << 24 | buf[1] << 16 | buf[2] << 8 | buf[3] );
 			fprintf(stderr, "[pwmclk INFO] Mem Base is 0x%x\n",mem_base);
 		}
@@ -92,8 +89,8 @@ uint32_t get_and_map_mem() {
 			mem_size = (buf[0] << 24 | buf[1] << 16 | buf[2] << 8 | buf[3] );
 			fprintf(stderr, "[pwmclk INFO] Mem Size is 0x%x\n",mem_size);	
 		}	
-        close(fd);
-        if(mem_base && mem_size) {
+		close(fd);
+		 if(mem_base && mem_size) {
 			if ((dev = open("/dev/mem", O_RDWR | O_SYNC) ) >= 0)     {
 				mem_map = (uint32_t *)mmap(NULL, mem_size, PROT_READ|PROT_WRITE, MAP_SHARED, dev, mem_base);
     			if (mem_map == MAP_FAILED) {
@@ -103,18 +100,18 @@ uint32_t get_and_map_mem() {
 			    	fprintf(stderr, "[pwmclk INFO] Mapped to memory\n");
     			}
 			} else {
-			    fprintf(stderr, "[pwmclk ERROR] Clock Not Set. Failure opening /dev/mem: %s\n", strerror(errno));
-    			map_err=3;
-    		}        
-    	} else {
+				fprintf(stderr, "[pwmclk ERROR] Clock Not Set. Failure opening /dev/mem: %s\n", strerror(errno));
+    				map_err=3;
+    			}        
+    		} else {
 			fprintf(stderr, "[pwmclk ERROR] Clock Not Set. Error reading memory settings from /sys/firmware/devicetree/base/soc/ranges: %s\n", strerror(errno));
-    		map_err=2;
-    	}
-    } else {
+    			map_err=2;
+    		}
+	} else {
 		fprintf(stderr, "[pwmclk ERROR] Clock Not Set. Failure opening /sys/firmware/devicetree/base/soc/ranges: %s\n", strerror(errno));
-    	map_err=1;
-    }
-    return map_err ? 0 : 1;
+		map_err=1;
+	}
+	 return map_err ? 0 : 1;
 }
 
 void start_pwmclk() {
@@ -122,11 +119,11 @@ void start_pwmclk() {
 	
 	current_reg=reg_read(mem_map + PWMCLK_REG_CTRL/4);
 
-	if(current_reg & PWMCLK_BUSY_BIT) {  															//clock is running!
-		fprintf(stderr, "[pwmclk INFO] Clock already running...\n");						//wait for clock to stop
+	if(current_reg & PWMCLK_BUSY_BIT) {  									//clock is running!
+		fprintf(stderr, "[pwmclk INFO] Clock already running...\n");						
 	} else {
 		fprintf(stderr, "[pwmclk INFO] Starting clock...\n");
-		reg_write(mem_map + PWMCLK_REG_DIV/4, PWMCLK_PASSWORD | PWMCLK_SPEED); 							//set clock
+		reg_write(mem_map + PWMCLK_REG_DIV/4, PWMCLK_PASSWORD | PWMCLK_SPEED); 				//set clock
 		reg_write(mem_map + PWMCLK_REG_CTRL/4, PWMCLK_PASSWORD | PWMCLK_ENABLE_BIT | PWMCLK_OSC_BIT); 	//start clock
 		while(!(current_reg & PWMCLK_BUSY_BIT)) {
 			current_reg=reg_read(mem_map + PWMCLK_REG_CTRL/4);
